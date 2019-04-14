7458823fa01a9404e639fadb3e3adecba2ff52e3
erqidiy <erqitao@qq.com>
2019-04-14 10:40:59 +0800

    improved prompt infomation

--- chapter7/greeter.py
+++ chapter7/greeter.py
@@ -1,2 +1,4 @@
-name = input('Please enter your name: ')
+prompt = "If you tell us who are you, we can personalize the messages you see."
+prompt += "\nWhat your first name? "
+first_name = input(prompt)
 print('Hello', name, '!')
