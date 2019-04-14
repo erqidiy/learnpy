b6f9330e5ac3e7a8569dcba2c5d46101429c0055
erqidiy <erqitao@qq.com>
2019-04-14 10:43:43 +0800

    bug fixed

--- chapter7/greeter.py
+++ chapter7/greeter.py
@@ -1,4 +1,4 @@
 prompt = "If you tell us who are you, we can personalize the messages you see."
 prompt += "\nWhat your first name? "
 first_name = input(prompt)
-print('Hello', name, '!')
+print('Hello', first_name, '!')
